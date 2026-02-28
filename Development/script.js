c3b89947f077418501ca5277c520081a8c67abcd
megabyte28 <mridulbhatia2008@gmail.com>
2026-03-01 01:36:21 +0530

    .

--- Development/script.js
+++ Development/script.js
@@ -1,3 +1,6 @@
+const protocol = new pmtiles.Protocol();
+maplibregl.addProtocol("pmtiles", protocol.add);
+
 const map=new maplibregl.Map({
     container:'map',
     center:[77.2093,28.6139],
@@ -7,42 +10,42 @@ const map=new maplibregl.Map({
     //0--whole map
     //11-- city levels
     //18-- point levels
-})
+});
 
-Map.on('load',()=>{
-    map.addSource('safety-data',{type:'vector',url:'pmtiles://path/to/safety-score.pmtiles'})
-})
+map.on('load',()=>{
+    map.addSource('safety-data',{type:'vector',url:'pmtiles://path/to/safety-score.pmtiles'});
+    
 
-Map.addlayer({
-    id:'safety-heatmap-layer',
-    type:'heatmap',
-    source:'safety-data',
-    'source-layer':'safety-score',
-    paint:{
-        'heatmap-weight':[
-        'interpolate',['linear']
-        ['get','safety_score'],
-        0,0,350,1],
-
-        'heatmap-intensity':[
+    map.addLayer({
+        id:'safety-heatmap-layer',
+        type:'heatmap',
+        source:'safety-data',
+        'source-layer':'safety-score',
+        paint:{
+            'heatmap-weight':[
             'interpolate',['linear'],
-            ['zoom'],0,1,15,3],
+            ['get','safety_score'],
+            0,0,350,1],
 
-        'heat-color':[
-            'interpolate',['linear'],['heatmap-density'],
-            0,'rgba(0,0,0,0)',
-            0.2,'rgb(255,0,0)',
-            0.4,'rgb(255,165,0)',
-            0.6,'rgb(255,255,0)',
-            1,'rgb(0,255,0)'],
+            'heatmap-intensity':[
+                'interpolate',['linear'],
+                ['zoom'],0,1,15,3],
 
-        'heatmap-radius':[
-            'interpolate',['linear'],
-            ['zoom'],0,2,15,20
-        ]
+            'heatmap-color':[
+                'interpolate',['linear'],['heatmap-density'],
+                0,'rgba(0,0,0,0)',
+                0.2,'rgb(255,0,0)',
+                0.4,'rgb(255,165,0)',
+                0.6,'rgb(255,255,0)',
+                1,'rgb(0,255,0)'],
+
+            'heatmap-radius':[
+                'interpolate',['linear'],
+                ['zoom'],0,2,15,20
+            ]
 
         
     }
-    
+});
 
-})
+});
